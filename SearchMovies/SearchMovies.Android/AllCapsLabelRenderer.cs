25fa9c0098b298fa13108a1eea2cc4842df05594
thaeliss <Linna@LAPTOP-CVAQ4SS6>
2018-01-10 06:25:22 +0100

    Finish custom renderer.

---
+++ SearchMovies/SearchMovies.Android/AllCapsLabelRenderer.cs
@@ -0,0 +1,24 @@
+﻿using SearchMovies.Data.CustomRenderer;
+using SearchMovies.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(AllCapsLabel), typeof(AllCapsLabelRenderer))]
+namespace SearchMovies.Droid
+{
+    class AllCapsLabelRenderer : LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.SetBackgroundColor(global::Android.Graphics.Color.Black);
+                Control.SetAllCaps(true);
+                Control.SetPaddingRelative(20,5,50,10);
+                Control.SetTextColor(global::Android.Graphics.Color.White);
+            }
+        }
+    }
+}
---
+++ SearchMovies/SearchMovies/Data/CustomRenderer/AllCapsLabel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SearchMovies.Data.CustomRenderer
+{
+    class AllCapsLabel : Xamarin.Forms.Label
+    {
+    }
+}
